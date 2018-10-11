e48b80948db298ed92c60fe943271d21345cec94
Hannes Petri <mail@hannesp.se>
2018-10-11 21:22:54 +0200

    Fixed bugs in search previews

--- back/index.js
+++ back/index.js
@@ -25,29 +25,26 @@ app.get("/api/entry/:entry/:defNo", (req, res) => {
 function preview(segments) {
   const cap = 100;
 
-  const lens = segments
-    .map((seg) => seg[1].length)
-    .reduce((accum, len) => [
-      ...accum,
-      accum.slice(-1)[0] + len
-    ], [0]);
-
+  const lens = segments.map(([, text]) => text.length);
+  const cumLens =lens.map((len, i, arr) => arr.slice(0, i+1).reduce((a, b) => a + b));
   
-  const limit = lens.find(len => len > cap);
-  const i = lens.indexOf(limit) - 1;
+  const limit = cumLens.find(len => len > cap);
+  const i = cumLens.indexOf(limit);
 
   if (i === -1) {
-    return segments;
+    return [segments, false];
   }
 
   const first = segments.slice(0, i);
   const last = segments[i];
 
-  const firstLen = first.map(([, text]) => text.length);
-  return [...first, [
+  const firstLen = first.map(([, text]) => text.length).reduce((a, b) => a + b);
+  const shortened = [...first, [
     last[0],
     last[1].substr(0, cap - firstLen)
   ]];
+
+  return [shortened, true];
 }
 
 app.get("/api/search/:query", (req, res) => {
@@ -58,11 +55,11 @@ app.get("/api/search/:query", (req, res) => {
     .slice(0, 25)
     .map(key => {
       const defs = index[key];
-      return defs.map(([, segs], i) => [
-        key,
-        i + 1,
-        preview(segs)
-      ]);
+      return defs.map(([, segs], i) => ({
+        entry: key,
+        defNo: i + 1,
+        preview: preview(segs)
+      }));
     })
     .reduce((accum, curr) => [...accum, ...curr], []);
 
