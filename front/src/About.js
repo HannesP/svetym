72ac7bd3caace965204f21dda0daa395b0c8538c
HannesP <mail@hannesp.se>
2019-04-17 22:02:59 +0200

    Converted About to using function keyword for consistency

--- front/src/About.js
+++ front/src/About.js
@@ -4,24 +4,26 @@ function emailAddress() {
   return "inXXXXetym.se".replace("XXXX", "fo@sv");
 }
 
-const About = () => (
-  <div>
-    <p>
-      This is an index of <cite>Svensk etymologisk ordbok</cite> by Elof
-      Hellquist, published in 1922. The digitalisation of the work was carried
-      out by <a href="http://runeberg.org/svetym/">Projekt Runeberg</a>; this
-      site is just a convenience for accessing it.
-    </p>
-    <p>
-      Contact: <a href={"mailto:" + emailAddress()}>{emailAddress()}</a>
-    </p>
-    <p>
-      GitHub:{" "}
-      <a href="https://github.com/HannesP/svetym">
-        https://github.com/HannesP/svetym
-      </a>
-    </p>
-  </div>
-);
+function About() {
+  return (
+    <div>
+      <p>
+        This is an index of <cite>Svensk etymologisk ordbok</cite> by Elof
+        Hellquist, published in 1922. The digitalisation of the work was carried
+        out by <a href="http://runeberg.org/svetym/">Projekt Runeberg</a>; this
+        site is just a convenience for accessing it.
+      </p>
+      <p>
+        Contact: <a href={"mailto:" + emailAddress()}>{emailAddress()}</a>
+      </p>
+      <p>
+        GitHub:{" "}
+        <a href="https://github.com/HannesP/svetym">
+          https://github.com/HannesP/svetym
+        </a>
+      </p>
+    </div>
+  );
+}
 
 export default About;
